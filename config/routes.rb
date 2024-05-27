ebd2570b1815debec121e4ff42469f6e88c32b1f
Jerome Serriere <155807922+JSerriere@users.noreply.github.com>
2024-05-27 14:49:15 +0200

    Add routes for bookmarks ans reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do  
   root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
 end
