9b1a72c665858e1c43f93cb76c9d7fbe7a4640b7
Jerome Serriere <155807922+JSerriere@users.noreply.github.com>
2024-05-27 18:02:57 +0200

    Add Create and params

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,24 @@
 class BookmarksController < ApplicationController
 
-    def new
-      @bookmark = Bookmark.new
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      @review = Review.new
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 
 end
