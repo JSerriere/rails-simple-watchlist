84897e3c6c43a088e4f2342fcfa9e93a0d86cd74
Jerome Serriere <155807922+JSerriere@users.noreply.github.com>
2024-05-27 15:03:40 +0200

    Add create

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,4 +13,13 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 end
