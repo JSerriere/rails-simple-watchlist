fcd984d14934c6234084289f2a5f43bd5ef18b75
Jerome Serriere <155807922+JSerriere@users.noreply.github.com>
2024-05-27 14:58:57 +0200

    Add Show

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,12 @@
 class ListsController < ApplicationController
     
-    def index
-      @lists = List.all
-    end
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+    @review = Review.new(list: @list)
+  end
+
 end
