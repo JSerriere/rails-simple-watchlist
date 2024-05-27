63fafa93610ef5ed97eaf3380d471ff2aa52b17b
Jerome Serriere <155807922+JSerriere@users.noreply.github.com>
2024-05-27 14:14:33 +0200

    Added tables original_title, original_language and release_date to movie model

--- db/migrate/20240524120428_create_movies.rb
+++ db/migrate/20240524120428_create_movies.rb
@@ -2,6 +2,9 @@ class CreateMovies < ActiveRecord::Migration[7.1]
   def change
     create_table :movies do |t|
       t.string :title
+      t.string :original_title
+      t.string :original_language
+      t.date :release_date
       t.text :overview
       t.string :poster_url
       t.float :rating
